4f815f4a9c9af0a561e5247afb7f87c531d7dc05
Kevin Wenger <wenger.kev@gmail.com>
2023-05-24 14:35:50 +0200

    keep cache enable by default

--- web/sites/default/settings.development.php
+++ web/sites/default/settings.development.php
@@ -30,24 +30,18 @@ $settings['file_private_path'] = '/var/www/web/sites/default/files/private';
  *
  * Disable all caches capabilities.
  */
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
-$settings['cache']['bins']['page'] = 'cache.backend.null';
-$config['system.logging']['error_level'] = 'all';
-$config['system.performance']['cache']['page']['use_internal'] = FALSE;
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['css']['gzip'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
-$config['system.performance']['js']['gzip'] = FALSE;
-$config['system.performance']['response']['gzip'] = FALSE;
-$config['views.settings']['ui']['show']['sql_query']['enabled'] = TRUE;
-$config['views.settings']['ui']['show']['performance_statistics'] = TRUE;
-$config['devel.settings']['devel_dumper'] = 'var_dumper';
-$config['devel.settings']['error_handlers'] = [4 => '4'];
-$config['devel.toolbar.settings']['toolbar_items'] = [
-  'devel.cache_clear', 'devel.container_info.service', 'devel.field_info_page', 'devel.run_cron', 'devel.admin_settings_link',
-  'devel.menu_rebuild', 'devel.reinstall', 'devel.route_info', 'devel.configs_list',
-];
+// $settings['cache']['bins']['render'] = 'cache.backend.null';
+// $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+// $settings['cache']['bins']['page'] = 'cache.backend.null';
+// $config['system.logging']['error_level'] = 'all';
+// $config['system.performance']['cache']['page']['use_internal'] = FALSE;
+// $config['system.performance']['css']['preprocess'] = FALSE;
+// $config['system.performance']['css']['gzip'] = FALSE;
+// $config['system.performance']['js']['preprocess'] = FALSE;
+// $config['system.performance']['js']['gzip'] = FALSE;
+// $config['system.performance']['response']['gzip'] = FALSE;
+// $config['views.settings']['ui']['show']['sql_query']['enabled'] = TRUE;
+// $config['views.settings']['ui']['show']['performance_statistics'] = TRUE;
 
 /**
  * Load services definition file.
