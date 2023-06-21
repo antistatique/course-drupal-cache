da03ec6045daa7b63bf51d85ff7d2c53341ca2c2
Kevin Wenger <wenger.kev@gmail.com>
2023-06-21 14:32:43 +0200

    init challenge-04

---
+++ challenges/challenge-04/README.md
@@ -0,0 +1,14 @@
+# Challenge #04 - Challenge 04 - Per user Role Cache
+
+Found a solution to display different recipes based on the current user role.
+Alter the existing Block `PerRoleRecipesBlock in order to display the following Recipes per Roles:
+
+| 🔒 Role            | 🍜 Recipes       |
+|--------------------|------------------|
+| Anonymous          | Easy             |
+| Authenticated User | Easy             |
+| Editor             | Medium           |
+| Author             | Medium           |
+| Administrator      | Hard             |
+
+See instructions on 👉 https://www.notion.so/Challenge-04-Per-user-Role-Cache-ac02bbc8511c4f86ae0eec3e37882835
---
+++ challenges/challenge-04/module/challenge_04/src/Plugin/Block/PerRoleRecipesBlock.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\challenge_04\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\State\StateInterface;
+
+/**
+ * Per Role Recipes Collection block.
+ *
+ * @Block(
+ *   id="challenge_04_recipes_block",
+ *   admin_label = @Translation("Per Role Recipes Collection block")
+ * )
+ */
+class PerRoleRecipesBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $nodeStorage;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, AccountInterface $currentUser) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->nodeStorage = $entity_type_manager->getStorage('node');
+    $this->entityTypeManager = $entity_type_manager;
+    $this->currentUser = $currentUser;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    // Instantiates this form class.
+    return new static(
+    // Load the service required to construct this class.
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $renderer = [
+      '#theme' => 'challenge_04___recipes_collection_block',
+      '#recipes' => [],
+    ];
+
+    $query = $this->nodeStorage->getQuery()
+      ->accessCheck(TRUE)
+      ->condition('type', 'recipe')
+      ->condition('status', 1)
+      ->sort('created', 'DESC');
+
+    $difficulty = match (TRUE) {
+      $this->currentUser->isAuthenticated() && !empty(array_intersect(['administrator'], $this->currentUser->getRoles())) => 'hard',
+      $this->currentUser->isAuthenticated() => 'medium',
+      $this->currentUser->isAnonymous() => 'easy',
+      default => 'easy',
+    };
+
+    $query->condition('field_difficulty', $difficulty, '=');
+
+    $nids = $query->execute();
+
+    if (!$nids || !\is_array($nids)) {
+      return $renderer;
+    }
+
+    $recipes = $this->nodeStorage->loadMultiple($nids);
+    foreach ($recipes as $recipe) {
+      $recipe_output = $this->entityTypeManager
+        ->getViewBuilder('node')
+        ->view($recipe, 'teaser');
+      $renderer['#recipes'][] = $recipe_output;
+    }
+
+    return $renderer;
+  }
+
+}
