121f74999d7d50296c3153ecfe370d1246677fe2
Kevin Wenger <wenger.kev@gmail.com>
2023-06-26 16:21:22 +0200

    init challenge-09

---
+++ challenges/challenge-09/README.md
@@ -0,0 +1,6 @@
+# Challenge #09 - Per Country Cache
+
+Alterate an existing Block RecipesByCountryBlock to serve all recipies for any country excepted for CH which should not see Desserts.
+
+See instructions on 👉 https://www.notion.so/Challenge-09-Per-Country-Cache-c9924453c30a41f6833b625cfd72cee3
+
---
+++ challenges/challenge-09/module/challenge_09/src/Plugin/Block/RecipesByCountryBlock.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\challenge_09\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\State\StateInterface;
+
+/**
+ * Recipes by Country block.
+ *
+ * @Block(
+ *   id="challenge_09_recipes_by_country_block",
+ *   admin_label = @Translation("Recipes by Country block")
+ * )
+ */
+class RecipesByCountryBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $nodeStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->nodeStorage = $entity_type_manager->getStorage('node');
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    // Instantiates this form class.
+    return new static(
+    // Load the service required to construct this class.
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      // Parent Menu block.
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $renderer = [
+      '#theme' => 'item_list',
+      '#items' => [],
+      '#cache' => [
+        'tags' => [
+          'node_list:recipe',
+        ],
+      ],
+    ];
+
+    $query = $this->nodeStorage->getQuery()
+      ->accessCheck(TRUE)
+      ->condition('type', 'recipe')
+      ->condition('status', 1)
+      ->sort('created', 'DESC');
+
+    $nids = $query->execute();
+
+    if (!$nids || !\is_array($nids)) {
+      return $renderer;
+    }
+
+    $recipes = $this->nodeStorage->loadMultiple($nids);
+    foreach ($recipes as $recipe) {
+      $recipe_output = $this->entityTypeManager
+        ->getViewBuilder('node')
+        ->view($recipe, 'teaser');
+      $renderer['#items'][] = $recipe_output;
+    }
+
+    return $renderer;
+  }
+
+}
